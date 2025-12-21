8b91b5d3cbf24de26f5a574d4d4d3cc27943542f
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-12-21 12:19:15 +0000

    Added APIs for watchlist

--- movies-api/api/userStore/index.js
+++ movies-api/api/userStore/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import UserStore from './userStoreModel';
-//import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -38,7 +37,7 @@ router.delete('/favourite', async (req, res) => {
     if (!req.user.isAuthenticated) {
         res.status(401).json({ msg: "Unauthorized" });
     }
-    console.log(req)
+
     const { movieId } = req.body;
 
     if (!movieId) {
@@ -53,6 +52,49 @@ router.delete('/favourite', async (req, res) => {
     );
 
     res.status(200).json(store?.favouritesList || []);
+    
+});
+
+
+
+router.post('/watchlist', async (req, res) => {
+    if (!req.user.isAuthenticated) {
+        res.status(401).json({ msg: "Unauthorized" });
+    }
+    const { movieId } = req.body;
+
+    if (!movieId) {
+        return res.status(400).json({ msg: 'movieId is required' });
+    }
+
+    const store = await UserStore.findOneAndUpdate(
+        { userId: req.user._id },
+        { $addToSet: { watchList: movieId } }, 
+        { new: true, upsert: true } 
+    );
+
+    res.status(200).json(store.watchList);
+});
+
+
+router.delete('/watchlist', async (req, res) => {
+    if (!req.user.isAuthenticated) {
+        res.status(401).json({ msg: "Unauthorized" });
+    }
+    const { movieId } = req.body;
+
+    if (!movieId) {
+        return res.status(400).json({ msg: 'movieId is required' });
+    }
+    
+
+    const store = await UserStore.findOneAndUpdate(
+        { userId: req.user._id },
+        { $pull: { watchList: movieId } }, 
+        { new: true }
+    );
+
+    res.status(200).json(store?.watchList || []);
     
 });
 
--- movies-api/api/userStore/userStoreModel.js
+++ movies-api/api/userStore/userStoreModel.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const UserStoreSchema = new Schema({
 favouritesList: { type: [String], required: false },
+watchList: { type: [String], required: false },
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User'
